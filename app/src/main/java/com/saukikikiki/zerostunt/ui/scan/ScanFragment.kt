12c9e26452174d62bd21e8bb36d9cd9e66dbddd7
SaukiFutaki <utaki.sauki19@gmail.com>
2024-12-03 18:57:32 +0700

    3-12

--- app/src/main/java/com/saukikikiki/zerostunt/ui/scan/ScanFragment.kt
+++ app/src/main/java/com/saukikikiki/zerostunt/ui/scan/ScanFragment.kt
@@ -54,6 +54,7 @@ class ScanFragment : Fragment() {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
+
                 }
             }
     }
